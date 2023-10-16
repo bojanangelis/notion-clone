23a33bba1901ca0ea6e5532f068ec49941c1acfb
bojanangelis <bojanangeleski98@gmail.com>
2023-10-16 18:50:58 +0200

    filly working sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -58,6 +58,17 @@ const Navigation = () => {
     }
   }
 
+  const collapse = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(true)
+      setIsResetting(true)
+      sidebarRef.current.style.width = '0'
+      navbarRef.current.style.setProperty('width', '100%')
+      navbarRef.current.style.setProperty('left', '0')
+      setTimeout(() => setIsResetting(false), 300)
+    }
+  }
+
   return (
     <>
       <aside
@@ -70,6 +81,7 @@ const Navigation = () => {
       >
         <div
           role="button"
+          onClick={collapse}
           className={cn(
             'h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition',
             isMobile && 'opacity-100',
@@ -99,7 +111,11 @@ const Navigation = () => {
       >
         <nav className="bg-transparent px-3 py-2 w-full">
           {isCollapsed && (
-            <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />
+            <MenuIcon
+              role="button"
+              onClick={resetWidth}
+              className="h-6 w-6 text-muted-foreground"
+            />
           )}
         </nav>
       </div>
